be8951a0e6adbbc16baef2a283dbec5de7b5befc
ConorobDIT <c14733295@mydit.ie>
2016-04-13 18:06:14 +0100

    Added files via upload

--- Gui.java
+++ Gui.java
@@ -1,18 +1,15 @@
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
-import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.io.PrintStream;
 import java.awt.event.ActionEvent;
-import javax.swing.JScrollBar;
 
-public class Gui extends Algorithm {
-
-	protected JFrame frame;
+public class Gui extends JFrame{
+	private JButton Button1;
 	private JTextArea textArea;
 	private Algorithm newAlgo;
 
@@ -31,29 +28,37 @@ public class Gui extends Algorithm {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 523, 422);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JButton btnNewButton = new JButton("Click Me");
-		btnNewButton.setBounds(185, 40, 100, 40);
-		frame.getContentPane().add(btnNewButton);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		this.setSize(500,500);
+		this.setLayout(new FlowLayout());
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				
-				Algorithm newAlgo = new Algorithm();
-				textArea = new JTextArea();
-				textArea.setEditable(false);
-				textArea.setLineWrap(true);
-				textArea.setWrapStyleWord(true);
-				textArea.setBounds(48, 91, 387, 279);
-				PrintStream printStream = new PrintStream(new TextOutput(textArea));
-				System.setOut(printStream);
-				System.setErr(printStream);
-				System.out.println("Started!");
-				Algorithm.results();
-			}
-		});
+		Button1 = new JButton("Click Me");
+		
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setSize(300, 200);		
+
+		this.add(Button1);
+		this.add(textArea);
+		
+		Button1.addActionListener(new ActionListener() {
+
+
+	        @Override
+			public void actionPerformed(ActionEvent e) {
+				Object eventSource = e.getSource();
+				if(eventSource == Button1){
+					Algorithm newAlgo = new Algorithm();
+					PrintStream printStream = new PrintStream(new TextOutput(textArea));
+					System.setOut(printStream);
+					System.setErr(printStream);
+					newAlgo.results();
+				}
+	        }
+
+	}
 	}
 }
